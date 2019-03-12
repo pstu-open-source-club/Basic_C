30e70eea08187d7f4e23a521dee6f78f8ccf80bd
Yakin Rubaiyat <yaken.rubaiat11@gmail.com>
2019-03-12 09:10:02 +0600

    Add files via upload

---
+++ Array Fill II.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[2000],i,j;
+
+    scanf("%d",&n);
+
+    for(i=0;i<1000; )
+    {
+        for(j=0;j<n;i++,j++){
+            a[i]=j;
+
+        }
+    }
+    for(i=0;i<1000;i++)
+        printf("N[%d] = %d\n",i,a[i]);
+    return 0;
+}
